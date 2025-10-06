a4fc860d2e6a71eb340d988e05268e641eff2b52
waste3d <kolya@waste3d.su>
2025-10-06 15:41:19 +0300

    v1

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os/exec"
+	"sync"
 
+	"github.com/creack/pty"
 	"github.com/gorilla/websocket"
 )
 
@@ -15,9 +19,67 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func handleWebsocket(w http.ResponseWriter, r *http.Request) {
+func handleWebsocket(w http.ResponseWriter, r *http.Request, command []string) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Upgrader error:", err)
 	}
+	defer conn.Close()
+
+	cmd := exec.Command(command[0], command[1:]...)
+
+	// Запускаем команду в псевдотерминале (PTY)
+	ptmx, err := pty.Start(cmd)
+	if err != nil {
+		log.Println("Failed to start pty:", err)
+		conn.WriteMessage(websocket.TextMessage, []byte("Error starting command."))
+		return
+	}
+	defer ptmx.Close()
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		wsWriter := &wsWrapper{
+			conn: conn,
+		}
+		if _, err := io.Copy(wsWriter, ptmx); err != nil {
+			log.Println("Error copying from pty to websocket:", err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("Error reading from websocket:", err)
+				break
+			}
+
+			if _, err := ptmx.Write(message); err != nil {
+				log.Println("Error writing to pty:", err)
+				break
+			}
+		}
+	}()
+
+	wg.Wait()
+	log.Println("Client disconnected.")
+}
+
+type wsWrapper struct {
+	conn *websocket.Conn
+}
+
+func (w *wsWrapper) Write(p []byte) (n int, err error) {
+	err = w.conn.WriteMessage(websocket.BinaryMessage, p)
+	if err != nil {
+		return 0, err
+	}
+	return len(p), nil
 }
